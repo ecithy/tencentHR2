54332c4a54747a1ef872cb0d56d99790f70d538e
wulei <598779784@qq.com>
2019-04-16 00:44:51 +0800

    first commit

---
+++ tencent/middlewares.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your spider middleware
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/spider-middleware.html
+
+from scrapy import signals
+import random
+
+
+def get_ua():
+	first_num = random.randint(55, 62)
+	third_num = random.randint(0, 3200)
+	fourth_num = random.randint(0, 140)
+	os_type = [
+		'(Windows NT 6.1; WOW64)', '(Windows NT 10.0; WOW64)', '(X11; Linux x86_64)',
+		'(Macintosh; Intel Mac OS X 10_12_6)'
+	]
+	chrome_version = 'Chrome/{}.0.{}.{}'.format(first_num, third_num, fourth_num)
+
+	ua = ' '.join(['Mozilla/5.0', random.choice(os_type), 'AppleWebKit/537.36',
+				   '(KHTML, like Gecko)', chrome_version, 'Safari/537.36']
+				  )
+	return ua
+
+
+class TencentSpiderMiddleware(object):
+	# Not all methods need to be defined. If a method is not defined,
+	# scrapy acts as if the spider middleware does not modify the
+	# passed objects.
+
+	@classmethod
+	def from_crawler(cls, crawler):
+		# This method is used by Scrapy to create your spiders.
+		s = cls()
+		crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+		return s
+
+	def process_spider_input(self, response, spider):
+		# Called for each response that goes through the spider
+		# middleware and into the spider.
+
+		# Should return None or raise an exception.
+		return None
+
+	def process_spider_output(self, response, result, spider):
+		# Called with the results returned from the Spider, after
+		# it has processed the response.
+
+		# Must return an iterable of Request, dict or Item objects.
+		for i in result:
+			yield i
+
+	def process_spider_exception(self, response, exception, spider):
+		# Called when a spider or process_spider_input() method
+		# (from other spider middleware) raises an exception.
+
+		# Should return either None or an iterable of Response, dict
+		# or Item objects.
+		pass
+
+	def process_start_requests(self, start_requests, spider):
+		# Called with the start requests of the spider, and works
+		# similarly to the process_spider_output() method, except
+		# that it doesn’t have a response associated.
+
+		# Must return only requests (not items).
+		for r in start_requests:
+			yield r
+
+	def spider_opened(self, spider):
+		spider.logger.info('Spider opened: %s' % spider.name)
+
+
+class RandomUserAgentMid:
+	def process_request(self, request, spider):
+		request.headers["User-Agent"] = get_ua()
+
+
+class ProxyMid:
+	def process_request(self, request, spider):
+		request.meta["proxy"] = "http://127.0.0.1:8000"
+
+
+class CheckUA:
+	def process_response(self, request, response, spider):
+		print(request.headers["User-Agent"], "*" * 100)
+		return response
